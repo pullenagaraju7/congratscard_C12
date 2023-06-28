ab7c927045e8ce4bd4f25adcd969085f644913b2
Nagaraju <pullenagaraju7@gmail.com>
2023-06-28 03:36:24 +0000

    congratscard_C12

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="container">
+    <h1 className="heading">Congratulations</h1>
+    <div className="card">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h1 className="name">Kiran V</h1>
+      <p className="location">
+        Vishnu Institute of Computer Education and Technology
+        <br />
+        Bhimavaram
+      </p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
